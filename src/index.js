01165c346b79f6a0ab22ea5d5d14285e32c01f6c
Sergejs Koreskovs <skoreskovs@evolution.com>
2023-03-20 13:01:41 +0200

    Domain sell template

--- src/index.js
+++ src/index.js
@@ -1,26 +1,31 @@
 // Test import of a JavaScript module
-import { example } from '@/js/example'
+// import { example } from '@/js/example'
 
 // Test import of an asset
-import webpackLogo from '@/images/webpack-logo.svg'
+import logo from '@/images/logo.svg'
 
 // Test import of styles
 import '@/styles/index.scss'
 
 // Appending to the DOM
-const logo = document.createElement('img')
-logo.src = webpackLogo
+const logoEl = document.querySelector('#img-main')
+logoEl.src = logo
 
-const heading = document.createElement('h1')
-heading.textContent = example()
+// const heading = document.createElement('h1')
+// heading.textContent = example()
 
-// Test a background image url in CSS
-const imageBackground = document.createElement('div')
-imageBackground.classList.add('image')
+// // Test a background image url in CSS
+// const imageBackground = document.createElement('div')
+// imageBackground.classList.add('image')
 
-// Test a public folder asset
-const imagePublic = document.createElement('img')
-imagePublic.src = '/assets/example.png'
+// // Test a public folder asset
+// const imagePublic = document.createElement('img')
+// imagePublic.src = '/assets/example.png'
 
-const app = document.querySelector('#root')
-app.append(logo, heading, imageBackground, imagePublic)
+// const app = document.querySelector('#root')
+// app.append(logo, heading, imageBackground, imagePublic)
+
+const domain = window.location.host
+const domainEl = document.querySelector('#domain')
+domainEl.innerHTML = 'Buy this domain -> ' + domain
+
